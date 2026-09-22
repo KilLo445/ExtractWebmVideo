56d21040cb3d8ac1596bb701f2c0e8aa40a9134d
KilLo445 <20kaedyn05@gmail.com>
2026-09-21 22:21:29 -0400

    Update icon

--- ExtractWebmVideo/Properties/AssemblyInfo.cs
+++ ExtractWebmVideo/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExtractWebmVideo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extract WebM files from a Just Dance AutoDance Video")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("KilLo")]
 [assembly: AssemblyProduct("ExtractWebmVideo")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
